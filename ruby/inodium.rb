3ca264a49ee59d98255be0c9278b5ebb9e446b67
Richard Forth <richard.forth@gmail.com>
2020-01-17 17:11:03 +0000

    Obsoleted a small method  and created RDOC documentation

--- ruby/inodium.rb
+++ ruby/inodium.rb
@@ -10,14 +10,20 @@ require 'find'
 # Description: Inode Hunter.
 #=--------------------------------------------------------=#
 
+# the successflag determines what happens on program exit, prevents duplicate reports being generated.
 @successflag = 0
 
+# A circular buffer class.
+# A CircularBuffer can only hold a fixed number of objects. The oldest item is removed to retain equiilibrium.
 class CircularBuffer
+
+  # initialize the object with a size
   def initialize(size:)
     @max_size = size
     @buffer = []
   end
 
+  # A shovel method used to add items to the CircularBuffer, note that if the buffer is full, the first item is removed. to make a space.
   def <<(value)
     if @buffer.size >= @max_size
       @buffer.shift
@@ -27,35 +33,34 @@ class CircularBuffer
     end
   end
 
+  # Returns the size of the buffer/
   def size
     @buffer.size
   end
 
+  # returns the highest "count" - each item in the buffer has a path, and a count.
   def highest
     return 0 if @buffer.length == 0
     @buffer.sort_by { |hsh| hsh[:count] }[-1][:count]
   end
 
+  # Return the unformatted buffer
   def report
     @buffer
   end
 end
 
-
-def is_folder(path)
-   File.directory?(path)
-end
-
-
+# is_dir? returns a boolean
 def is_dir?(dir)
   File.directory?(dir)
 end
 
+# is_file? returns a boolean
 def is_file?(path)
   File.file?(path)
 end
 
-
+# Produces a list of files and folders in a given dir
 def list_one_deep(dir)
   dir = "/" + dir  unless dir.start_with?("/")
   dir = dir + "/" unless dir.end_with?("/")
@@ -68,6 +73,7 @@ def list_one_deep(dir)
   fulldirs - [dir + ".", dir + ".."]
 end
 
+# A method that scans a directory and uses the CircularBuffer class as a buffer object
 def scan(dir, buffer)
   puts "Scan started at #{Time.now}."
   @skipped = []
@@ -97,6 +103,7 @@ end
 # Create a circular buffer of 20 items.
 myTopTwenty = CircularBuffer.new(size: 20)
 
+# This at_exit forces a report out even if we quit the program early (perhaps if it is taking too long to respond)
 at_exit do
   if @successflag == 1
     puts "\n\nDone.\n"
@@ -121,8 +128,8 @@ if ARGV.length != 1
 else
   fs = ARGV[0]
   # CHECK IF THE FOLDER IS A REAL EXISTING FOLDER, OR ABORT.
-  puts "FATAL: Not a valid folder," unless is_folder(fs)
-  exit unless is_folder(fs)
+  puts "FATAL: Not a valid folder," unless is_dir(fs)
+  exit unless is_dir(fs)
 end
 puts
 title = "Top 20 Directories in #{fs} for inodes"
